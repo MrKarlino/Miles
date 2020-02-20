e89db191a4fe41466e84c917becba7f1934949a4
Lina <linokarlino@mail.ru>
2020-02-20 13:58:20 +0300

    Добавлен код высчета милей

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int price = 3573;
+	int pricePerMile = 20;
+	int mileBonus = price/pricePerMile;
+	System.out.println(mileBonus);
+    }
+}
